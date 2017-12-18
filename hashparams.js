dd45e6e992327c6902ec827df940d034b64e5b38
Lee Spottiswood <me@lee.io>
2017-12-18 20:45:12 +0000

    add js

---
+++ hashparams.js
@@ -0,0 +1,50 @@
+var HashParameters = {
+    hashParams: {},
+    initialize: function() {
+        if (window.location.hash) {
+            var paramItems = (window.location.hash.substr(1)).split("&");
+
+            for (i = 0; i < paramItems.length; i++) {
+                var a = paramItems[i].split("=");
+                if (a[0] === undefined) {
+                    continue;
+                }
+
+                this.set(a[0], ((a[1] !== undefined) ? decodeURIComponent(a[1]) : ''));
+            }
+        }
+    },
+    get: function(key) {
+        if (this.hashParams[key] !== undefined) {
+            return this.hashParams[key];
+        }
+
+        return null;
+    },
+    set: function(key, value) {
+        this.hashParams[key] = value;
+        this.commit();
+    },
+    unset: function(key) {
+        if (hashParams[key] !== undefined) {
+            delete hashParams[key];
+        }
+
+        this.commit();
+    },
+    commit: function() {
+        var paramArray = [];
+        var obj = this;
+        Object.keys(this.hashParams).forEach(function(key,index) {
+            var paramStr = key;
+            if (obj.get(key) !== null) {
+                paramStr += "="+encodeURIComponent(obj.get(key));
+            }
+            paramArray.push(paramStr);
+        });
+
+        window.location.hash = "#"+paramArray.join('&');
+    }
+};
+
+HashParameters.initialize();
